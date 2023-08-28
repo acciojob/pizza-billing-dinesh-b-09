ee2372e3f404b12f9f6aee1c8049de178b871fc3
dinesh-b-09 <dineshbseervi@gmail.com>
2023-08-28 11:46:33 +0530

    attempt-1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,26 +9,43 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        this.price = isVeg ? 300 : 400;
+        this.bill = "Base Price Of The Pizza: " + this.price + "\n";
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(!bill.contains("Extra Cheese Added"))
+        {
+            this.price+=80;
+            bill+="Extra Cheese Added: 80\n";
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!bill.contains("Extra Toppings Added")) {
+            this.price += isVeg ? 70 : 120;
+            bill += "Extra Toppings Added: " + (isVeg ? "70" : "120") + "\n";
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!bill.contains("Paperbag Added")) {
+            this.price += 20;
+            bill += "Paperbag Added: 20\n";
+        }
     }
 
     public String getBill(){
         // your code goes here
+        bill += "Total Price: " + this.price + "\n";
         return this.bill;
     }
 }
